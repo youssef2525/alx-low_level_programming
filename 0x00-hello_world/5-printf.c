ad4f16e37f5e148947841ed2535370b5388b3418
Youssef YBT <youssef.benaleb25@gmail.com>
2023-06-16 00:37:54 +0100

    add

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 /**
- * print : with proper grammar, but the outcome is a piece of art
- *
- * Return:  0 
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
