68b16d4f213e73761fe0f8550e809f7addd3c55e
Youssef YBT <youssef.benaleb25@gmail.com>
2023-06-20 23:33:29 +0100

    add 0-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * print_sign - prints the sign of a number
- * 
  * Return: 1 and prints + if n is greater than zero
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
