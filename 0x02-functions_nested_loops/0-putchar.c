5a8d8d4100a653feb50e3a58bc90860d84d0097c
Youssef YBT <youssef.benaleb25@gmail.com>
2023-06-20 23:20:40 +0100

    add file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
 #include "main.h"
+
 /**
- * main - print _putchar
- * 
- * Return: 0
+ * main - Prints _putchar followed by a new line.
+ *
+ * Return: Always 0.
  */
-
-
 int main(void)
 {
 	_putchar('_');
@@ -17,5 +16,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
